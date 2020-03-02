c01f0b7fddba9dcc6ae6046e298fcdd34f83622f
이동영 <ldy@coinone.com>
2020-03-02 12:27:50 +0900

    Add singleton with parameter

--- patterns/src/test/kotlin/Singleton.kt
+++ patterns/src/test/kotlin/Singleton.kt
@@ -7,18 +7,51 @@ object PrinterDriver {
     }
 
     fun print(): PrinterDriver =
-        apply { println("Printing with object: $this") }
+            apply { println("Printing with object: $this") }
+}
+
+class SingletonWithParameter {
+    init {
+        println("Initializing with $this")
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SingletonWithParameter? = null
+
+        fun getInstance(context: String): SingletonWithParameter {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildSingletonWithParameter(context).also { INSTANCE = it }
+            }
+        }
+
+        private fun buildSingletonWithParameter(context: String): SingletonWithParameter {
+            println("print parameter $context")
+            return SingletonWithParameter()
+        }
+    }
 }
 
 class SingletonTest {
 
     @Test
-    fun Singleton() {
+    fun singletonByObject() {
         println("Start")
         val printerFirst = PrinterDriver.print()
         val printerSecond = PrinterDriver.print()
 
         assertThat(printerFirst).isSameAs(PrinterDriver)
         assertThat(printerSecond).isSameAs(PrinterDriver)
+    }
+
+    @Test
+    fun singletonWithCompanionObject() {
+        println("start")
+
+        val firstInstance = SingletonWithParameter.getInstance("context")
+        val secondInstance = SingletonWithParameter.getInstance("context")
+
+        assertThat(firstInstance).isSameAs(secondInstance)
+        assertThat(secondInstance).isSameAs(firstInstance)
     }
 }
